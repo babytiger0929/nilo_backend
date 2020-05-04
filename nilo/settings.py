0a8ce91b7f8dcb28b93ad3928ce57a03ae61240a
babytiger0929 <babytiger0929@outlook.com>
2020-05-04 14:24:27 -0700

    change database to db.sqlite3

--- nilo/settings.py
+++ nilo/settings.py
@@ -95,23 +95,23 @@ WSGI_APPLICATION = 'nilo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nilo_base',
-        'USER': 'nilo_db_user',
-        'PASSWORD': 'Nilodbp@sswOrd',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nilo_base',
+#         'USER': 'nilo_db_user',
+#         'PASSWORD': 'Nilodbp@sswOrd',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
